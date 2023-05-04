cfe6799b607cc773653f9022de951dc70f7fd2c9
pxtheticcat <serega.ivanovv2012@gmail.com>
2023-05-04 13:54:33 +0300

    Реализован набор тестов для класса CopySyntaxTree Протестировано копирование объектов классов Number, BinaryOperation, FunctionalCall и Variable с помощью класса CopySyntaxTree.

---
+++ mocha-ts/test/CST.spec.js
@@ -0,0 +1,53 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+const pattern_1 = require("../../pattern");
+describe("Класс CopySyntaxTree", () => {
+    describe("Копирование объекта класса Number", () => {
+        const num1 = new pattern_1.Number_(16.0);
+        const CST = new pattern_1.CopySyntaxTree();
+        const newExpr = num1.transform(CST);
+        it("Исходный объект и новый объект являются различными объектами", () => {
+            chai_1.assert.notEqual(num1, newExpr);
+        });
+        it("Исходный объект и новый объект имеют идентичные AST представления", () => {
+            chai_1.assert.deepStrictEqual(num1, newExpr);
+        });
+    });
+    describe("Копирование объекта класса BinaryOperation", () => {
+        const num1 = new pattern_1.Number_(16.0);
+        const num2 = new pattern_1.Number_(8.0);
+        const minus = new pattern_1.BinaryOperation(num1, pattern_1.Operations.MINUS, num2);
+        const CST = new pattern_1.CopySyntaxTree();
+        const newExpr = minus.transform(CST);
+        it("Исходный объект и новый объект являются различными объектами", () => {
+            chai_1.assert.notEqual(minus, newExpr);
+        });
+        it("Исходный объект и новый объект имеют идентичные AST представления", () => {
+            chai_1.assert.deepStrictEqual(minus, newExpr);
+        });
+    });
+    describe("Копирование объекта класса FunctionalCall", () => {
+        const num1 = new pattern_1.Number_(16.0);
+        const callSqrt = new pattern_1.FuntionalCall("sqrt", num1);
+        const CST = new pattern_1.CopySyntaxTree();
+        const newExpr = callSqrt.transform(CST);
+        it("Исходный объект и новый объект являются различными объектами", () => {
+            chai_1.assert.notEqual(callSqrt, newExpr);
+        });
+        it("Исходный объект и новый объект имеют идентичные AST представления", () => {
+            chai_1.assert.deepStrictEqual(callSqrt, newExpr);
+        });
+    });
+    describe("Копирование объекта класса Variable", () => {
+        const var1 = new pattern_1.Variable("name");
+        const CST = new pattern_1.CopySyntaxTree();
+        const newExpr = var1.transform(CST);
+        it("Исходный объект и новый объект являются различными объектами", () => {
+            chai_1.assert.notEqual(var1, newExpr);
+        });
+        it("Исходный объект и новый объект имеют идентичные AST представления", () => {
+            chai_1.assert.deepStrictEqual(var1, newExpr);
+        });
+    });
+});
