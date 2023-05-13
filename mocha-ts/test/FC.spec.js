b9028c59411e9fd6c38748fdfd71c7b485b8a4fb
Лев Тарасов <133116394+Tarasovvvv@users.noreply.github.com>
2023-05-13 19:27:23 +0400

    Update FC.spec.js

--- mocha-ts/test/FC.spec.js
+++ mocha-ts/test/FC.spec.js
@@ -10,17 +10,7 @@ describe("Класс FoldConstants", () => {
         chai_1.assert.deepStrictEqual(num1, newExpr);
     });
     it("Сворачивание констант для объекта типа BinaryOperation", () => {
-        const num1 = new pattern_1.Number_(64.0);
-        const callSqrt = new pattern_1.FuntionalCall("sqrt", num1);
-        const var1 = new pattern_1.Variable("var");
-        const plus = new pattern_1.BinaryOperation(callSqrt, pattern_1.Operations.PLUS, var1);
-        const FC = new pattern_1.FoldConstants();
-        const newExpr = plus.transform(FC);
-        const res = new pattern_1.BinaryOperation(new pattern_1.Number_(8.0), pattern_1.Operations.PLUS, new pattern_1.Variable("var"));
-        chai_1.assert.deepStrictEqual(newExpr, res);
-    });
-    it("Сворачивание констант для объекта типа FunctionalCall", () => {
-        const num1 = new pattern_1.Number_(32.0);
+        const num1
         const num2 = new pattern_1.Number_(16.0);
         const minus = new pattern_1.BinaryOperation(num1, pattern_1.Operations.MINUS, num2);
         const callSqrt = new pattern_1.FuntionalCall("sqrt", minus);
